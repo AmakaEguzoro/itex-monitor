cb85cfc9b1631fd7d601f971d9465a9751cdcbba
teembet <timbetekanem@gmail.com>
2021-09-28 10:16:27 +0100

    update

--- src/app/views/merchant/merchant.component.ts
+++ src/app/views/merchant/merchant.component.ts
@@ -9,7 +9,7 @@ import { ToastService } from "ng-uikit-pro-standard";
 })
 export class MerchantComponent implements OnInit {
   isData: boolean;
-  isLoading: boolean;
+  loading: boolean;
   date: string;
   status: any;
   arr: any[] = [];
@@ -17,20 +17,34 @@ export class MerchantComponent implements OnInit {
   details: any;
   unassignedstat: any[] = [];
   tableData: any[] = [];
-
+  masterSelected: boolean;
+  checklist: any;
+  checkedList: any;
   page = 1;
   limit = 50;
   serial = 0;
   isAdmin: boolean;
   isAgent: boolean;
-
+  error: boolean = false;
   start: string;
   end: string;
 
   merchants: any;
-  masterSelected:boolean;
-  checklist:any;
-  checkedList:any;
+  merchant: any;
+  // terminalId: any;
+
+  merchantId: any;
+  contactName: any;
+  email: any;
+  phone: any;
+  description: any;
+  fax: any;
+  organisation: any;
+  createdAt: any;
+  updatedAt: any;
+  id: any;
+  edit: boolean;
+
   constructor(
     private payvueservice: PayVueApiService,
     private toast: ToastService
@@ -38,14 +52,43 @@ export class MerchantComponent implements OnInit {
 
   ngOnInit() {
     this.getMerchants();
-    this.arr=[]
+  }
+  getUnassigned(status, id) {
+    if (status) {
+      if (this.arr.includes(id)) {
+        return;
+      } else {
+        this.arr.push({ id });
+      }
+    } else if (!status) {
+      this.arr = this.arr.filter((item) => item.id !== id);
+    }
+    console.log(this.arr, "arr");
+  }
+
+  all: boolean;
+  checkAll() {
+    for (var i = 0; i < this.merchants.length; i++) {
+      this.merchants[i].isSelected = this.masterSelected;
+      if (this.masterSelected) {
+        console.log("true");
+        this.arr.push({ id: this.merchants[i].merchantId });
+        // this.merchants.map(merchant=>{
+        //
+        // })
+      } else {
+        console.log("false");
+        this.arr = [];
+      }
+    }
+    console.log(this.arr, "array");
   }
 
   getMerchants() {
     this.isData = undefined;
 
     let page = this.page < 1 ? 1 : this.page;
-
+    this.loading = true;
     const apiURL = `merchant/get-all-merchants?size=4&page_num=1`;
 
     this.payvueservice
@@ -56,97 +99,234 @@ export class MerchantComponent implements OnInit {
           this.merchants = data.data.rows;
 
           this.isData = true;
-          this.isLoading = false;
+          this.loading = false;
         } else {
           this.isData = false;
-          this.isLoading = false;
+          this.loading = false;
           // this.itemCount = 1;
         }
       })
       .catch((error) => {
         console.log(error);
         this.isData = false;
-        this.isLoading = false;
+        this.loading = false;
+        // this.itemCount = 1;
+      });
+  }
+  getMerchant(modal, status) {
+    this.loading = true;
+    this.edit = status;
+    if (this.arr.length !== 1) {
+      this.toast.warning("Please select one merchant");
+      return;
+    }
+    this.isData = undefined;
+
+    let id = this.arr[0].id;
+
+    const apiURL = `merchant//get-all-merchant-contacts/${id}`;
+
+    this.payvueservice
+      .apiCall(apiURL)
+      .then((data) => {
+        if (data.status == 200) {
+          this.serial = 1 + (this.page - 1) * this.limit;
+          this.merchant = data.data;
+          modal.show();
+          this.toast.success(data.message);
+
+          console.log(this.merchant);
+          if (status) {
+            this.merchantId = this.merchant.merchantId;
+            this.contactName = this.merchant.contactName;
+            this.email = this.merchant.email;
+            this.phone = this.merchant.phone;
+            this.organisation = this.merchant.organisation;
+            this.fax = this.merchant.fax;
+            this.createdAt = this.merchant.createdAt;
+            this.description = this.merchant.description;
+            this.updatedAt = this.merchant.updatedAt;
+          }
+          this.isData = true;
+          this.loading = false;
+        } else {
+          this.isData = false;
+          this.loading = false;
+          // this.itemCount = 1;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.isData = false;
+        this.loading = false;
+        // this.itemCount = 1;
+      });
+  }
+  deleteMerchant() {
+    if (this.arr.length !== 1) {
+      this.toast.warning("Please select one merchant");
+      return;
+    }
+    this.isData = undefined;
+
+    let id = this.arr[0].id;
+
+    const apiURL = `merchant/delete/${id}`;
+
+    this.payvueservice
+      .apiCall(apiURL)
+      .then((data) => {
+        if (data.status == 200) {
+          this.serial = 1 + (this.page - 1) * this.limit;
+          this.getMerchants();
+
+          this.toast.success(data.message);
+        } else {
+          this.isData = false;
+          this.loading = false;
+          // this.itemCount = 1;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.isData = false;
+        this.loading = false;
         // this.itemCount = 1;
       });
   }
 
-
-  // checkUncheckAll() {
-  //   for (var i = 0; i < this.unassignedstat.length; i++) {
-  //     this.unassignedstat[i] = this.masterSelected;
-  //   }
-  //   // this.getCheckedItemList();
-  //   console.log(this.masterSelected)
-  // }
-
-  
-
-  // // Get List of Checked Items
-  // getCheckedItemList(){
-  //   this.checkedList = [];
-  //   for (var i = 0; i < this.checklist.length; i++) {
-  //     if(this.checklist[i].isSelected)
-  //     this.checkedList.push(this.checklist[i]);
-  //   }
-  //   this.checkedList = JSON.stringify(this.checkedList);
-  // }
-
-  getUnassigned(status,id){
-    if (status){
-      if(this.arr.includes(id)){
-        return
-      }else{
-        this.arr.push({id})
-      }
-
+  toogleStatus() {
+    if (this.arr.length !== 1) {
+      this.toast.warning("Please select one merchant");
+      return;
     }
-    else if(!status){
-   this.arr=   this.arr.filter((item)=>item.id!==id)
-    } 
-    console.log(this.arr,"arr")
-  }
- 
-  all:boolean
-  checkAll(){
-    for(var i=0; i<this.merchants.length;i++){
-      this.merchants[i].isSelected = this.masterSelected;
-      if(this.masterSelected){
-      console.log("true")
-      this.arr.push({id:this.merchants[i].merchantID})
-      // this.merchants.map(merchant=>{
-      //   
-      // })
-     
-      }
-      else{
-        console.log("false")
+    this.isData = undefined;
 
-      }
+    let id = this.arr[0].id;
+
+    const apiURL = `terminal/toggle-status/${id}`;
+
+    this.payvueservice
+      .apiCall(apiURL, "put")
+      .then((data) => {
+        if (data.status == 200) {
+          this.serial = 1 + (this.page - 1) * this.limit;
+          this.getMerchants();
+
+          this.toast.success(data.message);
+        } else {
+          this.isData = false;
+          this.loading = false;
+          // this.itemCount = 1;
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        this.isData = false;
+        this.loading = false;
+        // this.itemCount = 1;
+      });
+  }
+
+  addMerchant() {
+    if (!this.merchantId) this.error = true;
+    if (!this.contactName) this.error = true;
+    if (!this.email) this.error = true;
+    if (!this.phone) this.error = true;
+    if (!this.fax) this.error = true;
+    if (!this.description) this.error = true;
+    if (!this.organisation) this.error = true;
+    if (!this.createdAt) this.error = true;
+    if (!this.updatedAt) this.error = true;
+    if (this.error) {
+      this.toast.warning("Please recheck input fields");
+    } else {
+      this.isData = undefined;
+
+      this.loading = true;
+
+      const apiURL = `merchant/create`;
+      const form = {
+        merchantID: this.merchantId,
+        contactName: this.contactName,
+        email: this.email,
+        description: this.description,
+        phone: this.phone,
+        fax: this.fax,
+        organisation: this.organisation,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+      };
+      this.payvueservice
+        .apiCall(apiURL, "post", form)
+        .then((data) => {
+          if (data.status == 200) {
+            this.loading = true;
+            this.getMerchants();
+            this.toast.success(data.message);
+          } else {
+            this.isData = false;
+            this.loading = false;
+            // this.itemCount = 1;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isData = false;
+          this.loading = false;
+          // this.itemCount = 1;
+        });
     }
-    console.log(this.arr,"array")
   }
 
-  addMerchant(){
+  editMerchant() {
+    if (!this.merchantId) this.error = true;
+    if (!this.contactName) this.error = true;
+    if (!this.email) this.error = true;
+    if (!this.phone) this.error = true;
+    if (!this.fax) this.error = true;
+    if (!this.description) this.error = true;
+    if (!this.organisation) this.error = true;
+    if (!this.createdAt) this.error = true;
+    if (!this.updatedAt) this.error = true;
+    if (this.error) {
+      this.toast.warning("Please recheck input fields");
+    } else {
+      this.loading = true;
+      const form = {
+        id: this.id,
+        merchantId: this.merchantId,
+        contactName: this.contactName,
+        email: this.email,
+        description: this.description,
+        phone: this.phone,
+        fax: this.fax,
+        organisation: this.organisation,
+        createdAt: this.createdAt,
+        updatedAt: this.updatedAt,
+      };
+      const apiURL = `merchant/edit`;
 
-  }
+      this.payvueservice
+        .apiCall(apiURL, "put", form)
+        .then((data) => {
+          if (data.status == 200) {
+            this.loading = false;
+            this.getMerchants();
 
-  editMerchant(){
-
-  }
-  deleteMerchants(){
-
-  }
-  viewMerchant(){
-
-  }
-  restoreMerchant(){
-
-  }
-  deleteContact(){
-
-  }
-  editContact(){
-    
+            this.toast.success(data.message);
+          } else {
+            this.isData = false;
+            this.loading = false;
+            // this.itemCount = 1;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isData = false;
+          this.loading = false;
+          // this.itemCount = 1;
+        });
+    }
   }
 }
